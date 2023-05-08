52f49a4f7795885b670faaa8507929bbac73cc99
Artur Paschoal <128155308+paschoal-artur@users.noreply.github.com>
2023-05-08 14:00:26 +0000

    added code

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 """This program counts the number of vowels in a string."""
-
 def count_vowels(string:str) -> int:
-    """
-    Takes in a string and returns the number of vowels in the string.
+    vowel = ["a", "e", "i", "o", "u"]
+    count = 0
+    word = False
+    while word == False :
+        for i in range(len(string)) :
+            if string[i] in vowel :
+                count += 1
+    return count
 
-    Args:
-        string (str): The string to count vowels in.
 
-    Returns:
-        int: The number of vowels in the string.
-    """
+#if __name__ == "__main__" :
+    #palavra = str(input("Digite uma palavra: "))
+    #vowel = ["a", "e", "i", "o", "u"]
+    #count = 0
+    #for i in range(len(palavra)) :
+        #if palavra[i] in vowel :
+            #count += 1
+    #print(count)
